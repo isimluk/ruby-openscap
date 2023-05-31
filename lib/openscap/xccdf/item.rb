aa451dbde336b45a9e66668caf55c290f29bd7a7
Šimon Lukašík <isimluk@fedoraproject.org>
2023-05-31 11:06:05 +0200

    allow extraction of markup from items

--- lib/openscap/xccdf/item.rb
+++ lib/openscap/xccdf/item.rb
@@ -40,8 +40,12 @@ module OpenSCAP
         OpenSCAP::TextList.find_plaintext OpenSCAP.xccdf_item_get_title(@raw), lang: prefered_lang
       end
 
-      def description(prefered_lang = nil)
-        OpenSCAP::TextList.find_plaintext OpenSCAP.xccdf_item_get_description(@raw), lang: prefered_lang
+      def description(prefered_lang = nil, markup: false)
+        if markup
+          OpenSCAP::TextList.find_markup OpenSCAP.xccdf_item_get_description(@raw), lang: prefered_lang
+        else
+          OpenSCAP::TextList.find_plaintext OpenSCAP.xccdf_item_get_description(@raw), lang: prefered_lang
+        end
       end
 
       def rationale(prefered_lang = nil)
